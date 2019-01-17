fd988997bf87ae5cc1fc791bf09189f79367e281
fiscalcraft <mebius@bk.ru>
2019-01-18 01:47:59 +0300

    Исправлена ошибка в линкер-скрипт-файле
    Теперь с инициализированными переменными всё впорядке

--- Dozer/Main/Header/BLE/gecko_bglib.h
+++ Dozer/Main/Header/BLE/gecko_bglib.h
@@ -95,8 +95,8 @@
 #define BGLIB_DEFINE()                                      \
   struct gecko_cmd_packet _gecko_cmd_msg;                   \
   struct gecko_cmd_packet _gecko_rsp_msg;                   \
-  struct gecko_cmd_packet *gecko_cmd_msg;                   \
-  struct gecko_cmd_packet *gecko_rsp_msg;                   \
+  struct gecko_cmd_packet *gecko_cmd_msg = &_gecko_cmd_msg; \
+  struct gecko_cmd_packet *gecko_rsp_msg = &_gecko_rsp_msg; \
   void (*bglib_output)(uint32_t len1, uint8_t* data1);      \
   int32_t (*bglib_input)(uint32_t len1, uint8_t* data1);    \
   int32_t (*bglib_peek)(void);                              \
@@ -113,7 +113,7 @@ extern int    gecko_queue_r;
  * @param OFUNC
  * @param IFUNC
  */
-#define BGLIB_INITIALIZE(OFUNC, IFUNC) bglib_output = OFUNC; bglib_input = IFUNC; bglib_peek = NULL; gecko_cmd_msg = &_gecko_cmd_msg; gecko_rsp_msg = &_gecko_rsp_msg;
+#define BGLIB_INITIALIZE(OFUNC, IFUNC) bglib_output = OFUNC; bglib_input = IFUNC; bglib_peek = NULL;
 
 /**
  * Initialize BGLIB to support nonblocking mode
@@ -121,7 +121,7 @@ extern int    gecko_queue_r;
  * @param IFUNC
  * @param PFUNC peek function to check if there is data to be read from UART
  */
-#define BGLIB_INITIALIZE_NONBLOCK(OFUNC, IFUNC, PFUNC) bglib_output = OFUNC; bglib_input = IFUNC; bglib_peek = PFUNC; gecko_cmd_msg = &_gecko_cmd_msg; gecko_rsp_msg = &_gecko_rsp_msg;
+#define BGLIB_INITIALIZE_NONBLOCK(OFUNC, IFUNC, PFUNC) bglib_output = OFUNC; bglib_input = IFUNC; bglib_peek = PFUNC;
 
 extern void(*bglib_output)(uint32_t len1, uint8_t* data1);
 extern int32_t (*bglib_input)(uint32_t len1, uint8_t* data1);
--- Dozer/Main/Source/Proc/BLE.cpp
+++ Dozer/Main/Source/Proc/BLE.cpp
@@ -9,15 +9,29 @@
 /* Own header */
 #include "app.h"
 
+PACKSTRUCT(struct st_date_time
+{
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hours;
+	uint8_t minutes;
+	uint8_t seconds;
+});
+
 BGLIB_DEFINE();
+
+uint8_t dozer_name[] = "Hello, Dozer!";
 
 void BLEProc(void *Param)
 {
+	uint32_t flag = 0;
+	uint8_t flag2 = 0;
 	struct gecko_cmd_packet* evt;
 
   /* Initialize BGLIB with our output function for sending messages. */
 	BGLIB_INITIALIZE_NONBLOCK(BLEUartTx, BLEUartRx, BLEUartPeek);
-	
+
 	InitBLEUart(BGLIB_MSG_MAX_PAYLOAD+4);
 
   /* Reset NCP to ensure it gets into a defined state.
@@ -25,10 +39,24 @@ void BLEProc(void *Param)
 	gecko_cmd_system_reset(0);
 
 	while (1) {
+		static struct st_date_time date_time = {2019, 01, 12, 14, 31, 0};
 		/* Check for stack event. */
 		evt = gecko_peek_event();
 		// Run application and event handler.
 		appHandleEvents(evt);
+
+		
+		if (flag > Time) {
+			flag2 ^= 0xFF;
+			if (flag2) LED_WORK_ON; else LED_WORK_OFF;
+			date_time.seconds++;
+			if (date_time.seconds > 59) date_time.seconds = 0;
+			if (gecko_cmd_gatt_server_write_attribute_value(17, 0x0000, sizeof(struct st_date_time), (const uint8_t*)&date_time)->result != 0) LED_ERR_ON;
+			else LED_ERR_OFF;
+			vTaskDelay(MS_TO_TICK(50));
+			gecko_cmd_gatt_server_send_characteristic_notification(0xFF, 17, sizeof(struct st_date_time), (const uint8_t*)&date_time);
+		}
+		flag = Time;
 	}
 }
 
@@ -68,7 +96,7 @@ void appHandleEvents(struct gecko_cmd_packet *evt)
       /* Set advertising parameters. 100ms advertisement interval. All channels used.
        * The first two parameters are minimum and maximum advertising interval, both in
        * units of (milliseconds * 1.6). The third parameter '7' sets advertising on all channels. */
-//      gecko_cmd_le_gap_set_adv_parameters(160, 160, 7);
+      gecko_cmd_le_gap_set_adv_parameters(160, 160, 7);
 
       /* Start general advertising and enable connections. */
       gecko_cmd_le_gap_set_mode(le_gap_general_discoverable, le_gap_undirected_connectable);
